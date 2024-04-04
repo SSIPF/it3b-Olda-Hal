3607305044ea7fa23acd2fd6a408159e016026ac
Olda-hal <olo.ha@seznam.cz>
2024-04-04 09:03:05 +0200

    setup script foe easier

---
+++ setupgit.sh
@@ -0,0 +1,8 @@
+apt install vim
+git --config --global core.editor vim
+git --config --global user.email "olo.ha@seznam.cz"
+git --config --global user.name "Olda-hal"
+mkdir repo
+cd repo
+git clone git@github.com:SSIPF/it3b-Olda-Hal.git
+
